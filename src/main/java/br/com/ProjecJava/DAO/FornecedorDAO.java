154dd20fdea0b749f6a062f5ad96642ee3cf59ae
Itamar <Itamar@LAPTOP-15CUNNR7>
2017-09-03 19:46:29 -0300

    Fornecedor

--- src/main/java/br/com/ProjecJava/DAO/FornecedorDAO.java
+++ src/main/java/br/com/ProjecJava/DAO/FornecedorDAO.java
@@ -1,5 +1,49 @@
 package br.com.ProjecJava.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.ProjecJava.model.Fornecedor;
+
 public class FornecedorDAO {
 
+	private final Connection conex;
+	
+	public FornecedorDAO(Connection con) {
+		this.conex = con;
+	}
+	
+	public boolean inserir(Fornecedor fornecedor) throws SQLException {
+		String sql = "INSERT INTO FORNECEDOR (FOR_COD,FOR_NOME,FOR_CNPJ,FOR_ENDERECO_COD,FOR_TELEFONE,FOR_EMAIL)VALUES(SEQ_FORNECEDOR.nextval,?,?,?,?,?)";
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setString(1, fornecedor.getNome());
+		statement.setString(2, fornecedor.getCnpj());
+		statement.setInt(3, fornecedor.getEndereco().getCodigo());
+		statement.setString(4, fornecedor.getTelefone());
+		statement.setString(5, fornecedor.getEmail());
+		
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean alterar(String nome, String cnpj, int endereco, String telefone, String email) throws SQLException {
+		String sql = "UPDATE FORNECEDOR SET FOR_NOME = ?, FOR_CNPJ = ?, FOR_ENDERECO_COD = ?, FOR_TELEFONE = ?, FOR_EMAIL = ? WHERE FOR_NOME = ?";
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setString(1, nome);
+		statement.setString(2, cnpj);
+		statement.setInt(3, endereco);
+		statement.setString(4, telefone);
+		statement.setString(5, email);
+		statement.setString(6, nome);
+		
+		return statement.executeUpdate() > 0;
+	}
+	
+	public boolean excluir(String nome) throws SQLException {
+		String sql = "DELETE FORNECEDOR WHERE FOR_NOME = ?";
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setString(1, nome);
+		
+		return statement.executeUpdate() > 0;
+	}
 }
--- src/main/java/br/com/ProjecJava/service/FornecedorService.java
+++ src/main/java/br/com/ProjecJava/service/FornecedorService.java
@@ -8,12 +8,24 @@ import br.com.ProjecJava.jdbc.ConnectionPoolOracle;
 import br.com.ProjecJava.model.Fornecedor;
 
 public class FornecedorService {
- 
+
 	public void inserir(Fornecedor fornecedor) throws SQLException {
 		try (Connection conex = new ConnectionPoolOracle().getConnection()) {
 			new FornecedorDAO(conex).inserir(fornecedor);
 		}
 	}
-	
-	public void alterar()
+
+	public void alterar(String nome, String cnpj, int endereco, String telefone, String email)
+			throws SQLException {
+		try (Connection conex = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(conex).alterar(nome, cnpj, endereco, telefone, email);
+		}
+	}
+
+	public void excluir(String nome) throws SQLException {
+		try (Connection conex = new ConnectionPoolOracle().getConnection()) {
+			new FornecedorDAO(conex).excluir(nome);
+		}
+	}
+
 }
