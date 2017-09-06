cae567fdecd6c79b8a1575f74f7c8342a1eb0e58
Itamar <Itamar@LAPTOP-15CUNNR7>
2017-09-06 15:14:14 -0300

    itamar 06/09

--- src/main/java/br/com/ProjecJava/DAO/SuprimentoDAO.java
+++ src/main/java/br/com/ProjecJava/DAO/SuprimentoDAO.java
@@ -1,5 +1,52 @@
 package br.com.ProjecJava.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.ProjecJava.model.Suprimento;
+
 public class SuprimentoDAO {
 
+	private final Connection conex;
+	
+	public SuprimentoDAO(Connection con) {
+		this.conex = con;
+	}
+	public boolean inserir(Suprimento suprimento) throws SQLException {
+		String sql = "INSERT INTO SUPRIMENTO(SUPRI_COD,SUPRI_NOME,SUPRI_TIPO_UND_COD,SUPRI_QUANTIDADE,SUPRI_CUSTO,SUPRI_FORNECEDOR,SUPRI_MARCA_COD) VALUES(SEQ_SUPRIMENTO.nextval,?,?,?,?,?,?)";
+		
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setString(1, suprimento.getNome());
+		statement.setInt(2, suprimento.getTipoUnidade().getCodigo());
+		statement.setInt(3, suprimento.getQuantidade());
+		statement.setDouble(4, suprimento.getCusto());
+		statement.setInt(5, suprimento.getFornecedor().getCodigo());
+		statement.setInt(6, suprimento.getMarca().getCodigo());
+		
+		return statement.executeUpdate() > 0;
+	}
+	public boolean alterar(Suprimento suprimento) throws SQLException {
+		String sql = "UPDATE SUPRIMENTO SET SUPRI_NOME = ?, SUPRI_TIPO_UND_COD = ?, SUPRI_QUANTIDADE = ?, SUPRI_CUSTO = ?, SUPRI_FORNECEDOR = ?, SUPRI_MARCA_COD = ? WHERE SUPRI_COD = ?";
+		
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setString(1, suprimento.getNome());
+		statement.setInt(2, suprimento.getTipoUnidade().getCodigo());
+		statement.setInt(3, suprimento.getQuantidade());
+		statement.setDouble(4, suprimento.getCusto());
+		statement.setInt(5, suprimento.getFornecedor().getCodigo());
+		statement.setInt(6, suprimento.getMarca().getCodigo());
+		statement.setInt(7, suprimento.getCodigo());
+		
+		return statement.executeUpdate() > 0;
+	}
+	public boolean excluir(int codigo) throws SQLException {
+		String sql = "DELETE EMPRESA WHERE SUPRI_COD = ?";
+		
+		PreparedStatement statement = conex.prepareStatement(sql);
+		statement.setInt(1, codigo);
+		
+		return statement.executeUpdate() > 0;
+	}
+	
 }
--- src/main/java/br/com/ProjecJava/service/FornecedorService.java
+++ src/main/java/br/com/ProjecJava/service/FornecedorService.java
@@ -1,4 +1,4 @@
-package br.com.ProjecJava.service;
+ package br.com.ProjecJava.service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
